7878a3d34d4b099460fe108e77069bee786167c3
ynon <ynon@tocode.co.il>
2025-05-12 11:29:38 +0300

    fix homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
-      <Link href="/blog">Visit my blog</Link>
+      
     </h1>
+    <p><Link href="/blog">Visit my blog</Link></p>
+    </>
+    
   )
 }
